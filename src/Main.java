4c7cc6475efaf794bf45649d2f1e1673b26e5986
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-04-07 16:44:17 -0300

    Operadores

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        
+        int anoDeLancamento = 2022;
+        
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+    
+        // Chamada de métodos
+        operadores();
     }
+
+    public static void operadores() {
+        int a = 10;
+        int b = 20;
+
+        // Operadores Aritméticos
+        System.out.println("Soma: " + (a + b));
+        System.out.println("Subtração: " + (a - b));
+        System.out.println("Multiplicação: " + (a * b));
+        System.out.println("Divisão: " + (a / b));
+        System.out.println("Módulo: " + (a % b));
+
+        // Operadores Relacionais
+        System.out.println("Igual: " + (a == b));
+        System.out.println("Diferente: " + (a != b));
+        System.out.println("Maior: " + (a > b));
+        System.out.println("Menor: " + (a < b));
+    }
+
 }
