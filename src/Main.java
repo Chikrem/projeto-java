2ee14ed2858239cf35770ece71a7e73c82fc4874
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-04-07 16:58:28 -0300

    Trabalhando com textos

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,21 @@ public class Main {
         int anoDeLancamento = 2022;
         
         System.out.println("Ano de Lançamento: " + anoDeLancamento);
-    
+        
+        String sinopse;
+        
+        sinopse = """
+            Filme Top Gun
+            Filme de aventura com galã dos anos 80
+            Muito bom!
+            Ano de Lançamento
+            """ + anoDeLancamento;
+            
+        System.out.println(sinopse);
+
         // Chamada de métodos
-        operadores();
+        // operadores();
+        // tiposDeDados();
     }
 
     public static void operadores() {
@@ -27,5 +39,23 @@ public class Main {
         System.out.println("Maior: " + (a > b));
         System.out.println("Menor: " + (a < b));
     }
+
+    public static void tiposDeDados() {
+        // Tipos primitivos
+        int numero = 10; // inteiro
+        double decimal = (10.5 + 55.5 + 10.9)/2; // ponto flutuante
+        char letra = 'A'; // caractere
+        boolean verdadeiro = true; // booleano
+        
+        // Tipos não primitivos
+        String texto = "Olá Mundo!"; // string  
+        
+        // Exibindo os valores
+        System.out.println("Número: " + numero);
+        System.out.println("Decimal: " + decimal);
+        System.out.println("Letra: " + letra);
+        System.out.println("Verdadeiro: " + verdadeiro);
+        System.out.println("Texto: " + texto);
+    }     
 
 }
